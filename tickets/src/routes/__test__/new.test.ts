6b37c9368150dfc77b2473816f8fa05c37635e75
stvbex <12851562+stvbex@users.noreply.github.com>
2025-03-26 18:51:31 +0200

    290

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app)
@@ -19,19 +20,67 @@ it('can only be accessed if the user is signed in', async () => {
 it('returns a status other than 401 if the user is signed in', async () => {
   const response = await request(app)
     .post('/api/tickets')
+    .set('Cookie', global.signin())
     .send({});
 
   expect(response.status).not.toEqual(401);
 });
 
 it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10
+    })
+    .expect(400);
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10
+    })
+    .expect(400);
+  
 });
 
 it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'defewfew',
+      price: -10
+    })
+    .expect(400);
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'defewfew'
+    })
+    .expect(400);
 });
 
 it('creates a ticket with valid inputs', async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
 
+  const title = 'daefefdf'
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price: 20
+    })
+    .expect(201)
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
 });
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,6 +2,7 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
+import jwt from 'jsonwebtoken'
 
 declare global {
   var signin: () => string[];
@@ -35,17 +36,24 @@ afterAll(async () => {
   await mongoose.connection.close();
 })
 
-global.signin = async () => {
+global.signin = () => {
   // Build a JTW payload. { id, email }
-
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com'
+  };
 
   // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
   // Build session Object.
+  const session = { jwt: token };
 
   // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
 
   // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
 
   // Return a string thats the coockie with encoded data
   return [`session=${base64}`];
